19eb9ac47ed924de14fd784685d3a4d592559b4f
nora <sxj042@gmail.com>
2023-03-22 09:54:58 -0400

    added casinoaccount and tests

--- src/main/java/com/github/zipcodewilmington/games/gamblingGames/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/games/gamblingGames/slots/SlotsGame.java
@@ -1,7 +1,13 @@
 package com.github.zipcodewilmington.games.gamblingGames.slots;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame {
+    Object[] slot = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+
+
 }

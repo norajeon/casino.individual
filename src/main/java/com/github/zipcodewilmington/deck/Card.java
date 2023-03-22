6a62ed75ea349a531f1fe4cbc70bc70e895ef72b
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-22 15:53:09 -0400

    Done cards

--- src/main/java/com/github/zipcodewilmington/deck/Card.java
+++ src/main/java/com/github/zipcodewilmington/deck/Card.java
@@ -1,4 +1,24 @@
 package com.github.zipcodewilmington.deck;
 
-public class Card {
+
+enum Rank{
+    ACE, TWO, THREE, FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING
+}
+enum Suit{
+    HEARTS, DIAMONDS,CLUBS,SPADES
+}
+class Card{
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+    public Rank getRank(){
+        return rank;
+    }
+    public Suit getSuit(){
+        return suit;
+    }
 }
--- src/main/java/com/github/zipcodewilmington/deck/Deck.java
+++ src/main/java/com/github/zipcodewilmington/deck/Deck.java
@@ -1,4 +1,18 @@
 package com.github.zipcodewilmington.deck;
+import java.util.List;
+import java.util.ArrayList;
+class Deck {
+    private List<Card> cards;
+    public Deck(){
+        cards = new ArrayList<>();
 
-public class Deck {
+        for(Rank rank : Rank.values()){
+            for(Suit suit : Suit.values()){
+                cards.add(new Card(rank, suit));
+            }
+        }
+    }
+    public int size(){
+        return cards.size();
+    }
 }

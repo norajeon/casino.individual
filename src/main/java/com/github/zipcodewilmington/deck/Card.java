309610a533fe14bd43fcd522d3740da8ee891670
Zachary <zachary@Zipcoders-MacBook-Pro-2.local>
2023-03-23 15:30:04 -0400

    moved enums inside class

--- src/main/java/com/github/zipcodewilmington/deck/Card.java
+++ src/main/java/com/github/zipcodewilmington/deck/Card.java
@@ -1,14 +1,15 @@
 package com.github.zipcodewilmington.deck;
 
+class Card{
+    private Rank rank;
+    private Suit suit;
 enum Rank{
      TWO, THREE, FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE
 }
 enum Suit{
     HEARTS, DIAMONDS,CLUBS,SPADES
 }
-class Card{
-    private Rank rank;
-    private Suit suit;
+
 
     public Card(Rank rank, Suit suit){
         this.rank = rank;

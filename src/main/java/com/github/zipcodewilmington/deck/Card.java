3354cb151379d85db76be7d96e3158532b0c65d4
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-23 15:14:42 -0400

    Deck and Yablon(not done)

--- src/main/java/com/github/zipcodewilmington/deck/Card.java
+++ src/main/java/com/github/zipcodewilmington/deck/Card.java
@@ -2,7 +2,7 @@ package com.github.zipcodewilmington.deck;
 
 
 enum Rank{
-    ACE, TWO, THREE, FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING
+     TWO, THREE, FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE
 }
 enum Suit{
     HEARTS, DIAMONDS,CLUBS,SPADES
